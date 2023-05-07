158a94cc742c0af8143648a4bae55b412823fb60
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-05-07 21:41:46 +0100

    Added the UserCreationForm

--- Users/forms.py
+++ Users/forms.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 '''
     UserRegistration
@@ -12,12 +15,34 @@ from django import forms
     -password
 '''
 
+# class UserRegistrationForm(forms.Form):
+#     username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+#     email = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+#     password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+#     confirm = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
 
-class UserRegistrationForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
-    email = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
-    confirm = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+
+class UserRegistrationForm(UserCreationForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm,self).__init__(*args, **kwargs)
+        self.fields["username"].label="Your Username"
+        self.fields["username"].widget=forms.TextInput(attrs={"class":"form-control"})
+    
+        self.fields["email"].label="Your Email"
+        self.fields["email"].widget=forms.TextInput(attrs={"class":"form-control"})
+    
+        self.fields["password1"].label="Your Password"
+        self.fields["password1"].widget=forms.PasswordInput(attrs={"class":"form-control"})
+    
+        self.fields["password2"].label="Confirm Password"
+        self.fields["password2"].widget=forms.PasswordInput(attrs={"class":"form-control"})
+    
+    
+    class Meta:
+        model = User
+        fields=["username", "email", "password1", "password2"]
+
 
 
 
--- Users/views.py
+++ Users/views.py
@@ -1,29 +1,24 @@
 from django.shortcuts import render, redirect
+
+from Posts.forms import PostCreationForm
 from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
 
 def sign_up(request):
     form = UserRegistrationForm()
     if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password = request.POST["password"]
-        confirm = request.POST["confirm"]
-        
-        if password == confirm:
-            new_user = User.objects.create_user(
-                username=username,
-                email=email,
-            )
-            new_user.set_password(password)
-            
-            new_user.save()
-            
-            return redirect('post_home')
-    
+       form = UserRegistrationForm(request.POST)
+       
+       if form.is_valid():
+           form.save()
+           
+           messages.success(request,"User Created Sucessfully")
+           
+           return redirect('login')
     context ={
         'form': form
     }
@@ -50,4 +45,37 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('post_home')
+
+
+
+
+
+
+
+'''Basic way to create a signup form in django'''
+# def sign_up(request):
+#     form = UserRegistrationForm()
+#     if request.method == "POST":
+#         username = request.POST["username"]
+#         email = request.POST["email"]
+#         password = request.POST["password"]
+#         confirm = request.POST["confirm"]
+        
+#         if password == confirm:
+#             new_user = User.objects.create_user(
+#                 username=username,
+#                 email=email,
+#             )
+#             new_user.set_password(password)
+            
+#             new_user.save()
+            
+#             return redirect('post_home')
+    
+#     context ={
+#         'form': form
+#     }
+#     return render(request, 'users/signup.html', context)
+
+
 

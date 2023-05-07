9c777dafe06b5075771534eeaa82305b21042cff
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-05-07 15:44:09 +0100

    Adding Django Model forms for Post Creation

--- Posts/views.py
+++ Posts/views.py
@@ -43,18 +43,44 @@ def create_post(request):
     return render (request,"create_post.html", context)
     
     
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
     
-    # tittle = (data['tittle'])
-    #     content = (data['content'])
-    #     author = (data['author'])
-        
-    #     new_post = Post(
-    #         tittle = tittle,
-    #         content = content,
-    #         author = author
-    #     )
-    #     new_post.save()
+    context = {"post": post}
+    return render(request, "post_detail.html",context)    
     
     
+def update_post(request, post_id):
+    post_to_update = Post.objects.get(pk=post_id)
     
+    form  = PostCreationForm(instance=post_to_update)
     
+    if request.method == 'POST':
+        form = PostCreationForm(instance=post_to_update,
+            data=request.POST, files=request.FILES
+        )
+    
+    if form.is_valid():
+        form.save()
+        return redirect("post_home")
+    
+    context = {
+        "form":form
+    }
+    return render(request, "update.html",context)    
+    
+
+
+# tittle = (data['tittle'])
+#     content = (data['content'])
+#     author = (data['author'])
+    
+#     new_post = Post(
+#         tittle = tittle,
+#         content = content,
+#         author = author
+#     )
+#     new_post.save()
+
+
+
